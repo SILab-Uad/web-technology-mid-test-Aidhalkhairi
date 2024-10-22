49ef63b4627a80b14bbf69f5e87564408d3bd816
Aidhalkhairi <2300016131@webmail.uad.ac.id>
2024-10-22 15:29:56 +0700

    Attempt - 16.10

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const generatePassword = (length, options) => {
     const numbers = "0123456789";
     const specialChars = "!@#$%^&*()";
 
+    // TODO: Create a variable for the character set based on selected options
     let characterSet = "";
     if (options.includeUppercase) characterSet += uppercase;
     if (options.includeLowercase) characterSet += lowercase;
@@ -17,6 +18,8 @@ const generatePassword = (length, options) => {
         throw new Error("No character sets selected");
     }
 
+
+    // TODO: Generate the password based on the selected criteria
     let password = "";
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characterSet.length);
@@ -26,6 +29,7 @@ const generatePassword = (length, options) => {
     return password;
 };
 
+    // TODO: Add event listener to the button to call generatePassword and display the output
 document.getElementById("generateButton").addEventListener("click", () => {
     const length =
 parseInt(document.getElementById("length").value);
@@ -43,13 +47,5 @@ document.getElementById("specialChars").Checked
     const password = generatePassword(length, options);
     document.getElementById("passwordOutput").innerText =
 password;
-});
-
-    // TODO: Create a variable for the character set based on selected options
-
-    // TODO: Generate the password based on the selected criteria
-   
-
-// TODO: Add event listener to the button to call generatePassword and display the output
-    
+    });    
 // BONUS: Implement the copy to clipboard functionality
